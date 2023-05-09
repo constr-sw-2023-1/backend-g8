409b8b19b7fdd270f08b4214868f77662fe58dfb
Tiago Villa <tiago.villa@edu.pucrs.br>
2023-05-09 20:15:00 -0300

    Adiciona schema inicial de Reserva

---
+++ models/Reservation.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+const ReservationSchema = new mongoose.Schema({
+  // TODO criar schema
+})
+
+module.exports = mongoose.model('Reservation', ReservationSchema)
