b91aec9acc3e04e6c4aed0a51fcc734b2d5b9601
Tiago Villa <tiago.villa@edu.pucrs.br>
2023-05-10 20:39:06 -0300

    Define schema da Reserva

--- models/Reservation.js
+++ models/Reservation.js
@@ -1,7 +1,28 @@
 const mongoose = require('mongoose')
 
 const ReservationSchema = new mongoose.Schema({
-  // TODO criar schema
+  dateSchedule: [
+    {
+      day: Date,
+      hour: String,
+      shift: String,
+    },
+  ],
+  dateReservationBegin: [
+    {
+      day: Date,
+      hour: Date,
+      shift: String,
+    },
+  ],
+  dateReservationEnd: [
+    {
+      day: Date,
+      hour: Date,
+      shift: String,
+    },
+  ],
+  resourceId: { type: mongoose.Schema.Types.ObjectId, ref: 'Resources' },
 })
 
 module.exports = mongoose.model('Reservation', ReservationSchema)
