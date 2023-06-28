0ca0a08b7a4e2e0c229f97fb0161d8bb38613286
guilherme stefani <stefaniguilherme2001@gmail.com>
2023-06-28 19:59:42 -0300

    update mock e dockerfile

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ WORKDIR /usr/src/app
 COPY package.json .
 RUN npm install
 
+RUN npm install -g nodemon
+
 # Bundle app source
 COPY server.js ./
 
--- mocks/resourceMock.js
+++ mocks/resourceMock.js
@@ -5,7 +5,7 @@ const mock = new MockAdapter(axios);
 
 const resourceEndpoint = "http://localhost:8084/resources/";
 const resourceId = "7920e5df-ee80-417e-a9a6-2115f840014b";
-const resourceData = { _id: resourceId, resourceName: "Laboratório 310" };
+const resourceData = { _id: resourceId, description: "Laboratório 310" };
 
 mock.onGet(`${resourceEndpoint}${resourceId}`).reply((config) => {
   console.log("Resposta simulada:", { success: true, data: resourceData });
